14ded49688962b2f8a93b4cfbbb0071b7bb588a3
BirdBoii <kiansp06@gmail.com>
2025-11-10 08:11:55 +0530

    Update DEPimplementation.py

--- DEPimplementation.py
+++ DEPimplementation.py
@@ -1,5 +1,10 @@
 '''
-Here we try to implement the whatever we have learnt in the thin airfoil theory to the DEP case
+Paper implementation of the DEP with the following considerations-
+- The airfoil is thin
+- It is a symmetric flat airfoil with a flap at the end which is responsible for the deflection of the jet of aiir
+- The angle of attacks must be low (Turbulent air has not been modelled
+- A rectangular Jet has been assumed to flow all over the surface of the airfoil
+- The contraction of this jet has not yet been taken into account
 '''
 
 import math
@@ -16,5 +21,6 @@ jetDeflection = 5 * math.pi / 180
 jDash = rhoJet * velJet * velJet * heightJet
 cj = jDash / (rhoinf * Vinf * Vinf * chord)
 cl = 2 * math.pi * (1 + 0.151 * math.sqrt(cj) + 0.219 * cj) * aoa + 2 * math.sqrt(math.pi * cj) * math.sqrt(1 + 0.151 * math.sqrt(cj) + 0.139 * cj) * jetDeflection
+
 
 print(cl)
