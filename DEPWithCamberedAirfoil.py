ee3e386c00ddd57fb1635c45ff842dffeba44f97
BirdBoii <kiansp06@gmail.com>
2025-11-12 18:16:37 +0530

    Update DEPWithCamberedAirfoil.py

--- DEPWithCamberedAirfoil.py
+++ DEPWithCamberedAirfoil.py
@@ -44,7 +44,7 @@ def returnCamberLine(camberLineFileLocation):
     return(xCamber, yCamber)
 
 #Angle of Attack in radians
-aoa = 10 * math.pi / 180
+aoa = 0 * math.pi / 180
 
 #Conditions
 fsv = 20 #free stream velocity in m/s
@@ -52,12 +52,12 @@ rho = 1.225
 cl = 0.0 #The coefficient of lift
 
 #Flaps
-percFlap = 50 #Percentage of the wing that is a flap
-flapAngle = 0 * math.pi / 180 #angle of the 
+percFlap = 30 #Percentage of the wing that is a flap
+flapAngle = 20 * math.pi / 180 #angle of the 
 #Jet
 heightJet = 0.3 #This is responsable for setting the height of the jet (this value should include the contraction effects of the wake)
-nExp = -2
-rhoJet = 2
+nExp = -0.36
+rhoJet = 1.3
 velocityJet = 50
 
 numberOfPoints = 0 # It is used to find the number of points in the CSV file which stores the chordline coordinates
@@ -106,7 +106,7 @@ anot = aoa - 1 / math.pi * anot
 for N in range(0, numberOfPoints):
     an.append(0)
     for n in range(0, numberOfPoints):
-        an[N] = an[N] - camberLineSlope[n] * math.cos(N*tetha[n]) * (tetha[n+1] - tetha[n])
+        an[N] = an[N] + camberLineSlope[n] * math.cos(N*tetha[n]) * (tetha[n+1] - tetha[n])
     an[N] = 2 / math.pi * an[N] 
 cl = math.pi * (2 * anot + an[1])
 cl2 = 0
@@ -138,7 +138,6 @@ for a in np.arange(0, mx + step, step):
 for a in range(0, int(mx / step)):
     jetCurveTetha.append(Acoef * pow(math.e, nExp * (1 / (x[a]) - airfoilLen) / airfoilLen))
     jetCurve.append((Acoef * airfoilLen / nExp) * (pow(math.e, nExp * (1 / (x[a]) - airfoilLen) / airfoilLen)) - yShift)
-    
 
 totalX = []
 totalX.extend(xCamber)
@@ -167,26 +166,23 @@ for a in range(0, numberOfPoints):
     for xPos in range(0, int(mx/step)):
         if(x[xPos] != (airfoilLen / 2 * (1 - math.cos(tetha[a])))):
             newAn[0] = newAn[0] +  1 / math.pi * (
-                deltaCj / (4 * math.pi * math.pi) * jetCurveTetha[xPos] * (x[xPos+1] - x[xPos]) / (airfoilLen / 2 * (1 - math.cos(tetha[a])) -x[xPos]) * (tetha[a+1] - tetha[a])
+                -deltaCj / (4 * math.pi) * jetCurveTetha[xPos] * (x[xPos+1] - x[xPos]) / (x[xPos] - airfoilLen / 2 * (1 - math.cos(tetha[a]))) * (tetha[a+1] - tetha[a])
                 )
         else:
             newAn[0] = newAn[0] +  1 / math.pi * (
-                deltaCj / (4 * math.pi * math.pi) * jetCurveTetha[xPos] * (tetha[a+1] - tetha[a])
-                )  
-        print(newAn[0])  
+                -deltaCj / (4 * math.pi) * jetCurveTetha[xPos] * (tetha[a+1] - tetha[a])
+                )   
     newAn[0] = newAn[0] - 1/math.pi * camberLineSlope[a] * (tetha[a+1] - tetha[a])
 newAn[0] = aoa + newAn[0]
-
 
 for a in range(1, numberOfPoints):
     for b in range(0, numberOfPoints):
         for xPos in range(0, int(mx/step)):
             newAn.append(2 / math.pi * (
-                deltaCj / ( math.pi * math.pi) * jetCurveTetha[xPos] * (x[xPos+1] - x[xPos]) / (0.000000001 + airfoilLen / 2 * (1 - math.cos(tetha[b])) - x[xPos] ) * (tetha[b+1] - tetha[b]) * math.cos(a * tetha[b]) 
+                -deltaCj / ( math.pi) * jetCurveTetha[xPos] * (x[xPos+1] - x[xPos]) / (0.000000001 +  x[xPos] - airfoilLen / 2 * (1 - math.cos(tetha[b]))  ) * (tetha[b+1] - tetha[b]) * math.cos(a * tetha[b]) 
             ))
-        newAn[a] = newAn[a] - 1/math.pi * camberLineSlope[b] * (tetha[b+1] - tetha[b]) * math.cos(tetha[b])
-        
+        newAn[a] = newAn[a] + 1/math.pi * camberLineSlope[b] * (tetha[b+1] - tetha[b]) * math.cos(tetha[b])
 clNew = math.pi * (2 * newAn[0] + newAn[1])
 
-print(clNew)
-print(cl)
+print("The coefficient of lift of our blown airfoil will be =", clNew)
+print("The coefficient of lift of our unblown airfoil will be =", cl)
